af126899ffb1c102c9a888079f74f2567baccc06
Philippe De Baer <philippe.debaer@infront.be>
2017-09-26 12:40:46 +0200

    Added Initialise.ps1

---
+++ ALMPOC.Tools/Scripts/Initialise.ps1
@@ -0,0 +1,4 @@
+﻿# Make sure you have PowerShell 5.0. you can find it here: https://www.microsoft.com/en-us/download/details.aspx?id=50395
+# enable running PS-scripts on local machine
+# run visual studio as admin
+Set-ExecutionPolicy RemoteSigned
