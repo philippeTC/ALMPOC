7ad952b853698f2117d022145e5b76292c1ea6ed
Hans Roelants <hans.roelants@gmail.com>
2017-09-27 14:08:46 +0200

    account.js

--- ALMPOC.CRM.Schema/WebResources/new_/account.js
+++ ALMPOC.CRM.Schema/WebResources/new_/account.js
@@ -4,6 +4,6 @@ Infront.account = {
 
     //*******************************Event Handlers**************************
     Form_Onload: function () {
-        alert("Form onload 2");
+        alert("Form onload");
     }
 }
